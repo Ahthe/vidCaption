de3ebf78b1841fdc8a3b5e90062aa60bfec45a1a
Shahnawaaz Meer <meersh@deakin.edu.au>
2024-08-25 15:19:54 +0930

    Front-End

--- frontend/features/landing-page/components/index.ts
+++ frontend/features/landing-page/components/index.ts
@@ -1,3 +1,2 @@
 export { Hero } from "./Hero";
-export { CommitmentSection } from "./CommitmentSection";
 export { FeaturesSection } from "./FeaturesSection";
--- frontend/features/landing-page/containers/LandingPageContainer.tsx
+++ frontend/features/landing-page/containers/LandingPageContainer.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Hero, CommitmentSection, FeaturesSection } from "../components";
+import { Hero, FeaturesSection } from "../components";
 
 export const LandingPageContainer = () => {
   return (
     <div className="flex justify-center">
       <div className="max-w-[1500px]">
         <Hero />
-        <CommitmentSection />
         <FeaturesSection />
       </div>
     </div>
