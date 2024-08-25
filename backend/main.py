2d4f2f112d1cce8c8b470b696ceef4679457336d
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-08-25 00:49:40 -0400

    created the format time func and gen subtitle func

--- backend/main.py
+++ backend/main.py
@@ -27,10 +27,42 @@ def transcribe(audio):
     return language, segments
 
 
+def format_time(seconds):
+
+    hours = math.floor(seconds / 3600)
+    seconds %= 3600
+    minutes = math.floor(seconds / 60)
+    seconds %= 60
+    milliseconds = round((seconds - math.floor(seconds)) * 1000)
+    seconds = math.floor(seconds)
+    formatted_time = f"{hours:02d}:{minutes:02d}:{seconds:01d},{milliseconds:03d}"
+
+    return formatted_time
+
+def generate_subtitle_file(language, segments):
+
+    subtitle_file = f"sub-{input_video_name}.{language}.srt"
+    text = ""
+    for index, segment in enumerate(segments):
+        segment_start = format_time(segment.start)
+        segment_end = format_time(segment.end)
+        text += f"{str(index+1)} \n"
+        text += f"{segment_start} --> {segment_end} \n"
+        text += f"{segment.text} \n"
+        text += "\n"
+        
+    f = open(subtitle_file, "w")
+    f.write(text)
+    f.close()
+
+    return subtitle_file
 
 def run():
 
     extracted_audio = extract_audio()
-
     language, segments = transcribe(audio=extracted_audio)
+    subtitle_file = generate_subtitle_file(
+    language=language,
+    segments=segments
+    )
 run()
