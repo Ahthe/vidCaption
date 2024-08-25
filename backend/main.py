a02f919cd0e0ec4c27a8f5ddd363e159b5c31817
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-08-25 00:25:54 -0400

    extract_audio() which is responsible for extracting the audio from the input video.

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,10 @@ from faster_whisper import WhisperModel
 
 input_video = "input.mp4"
 input_video_name = input_video.replace(".mp4", "")
+
+def extract_audio():
+    extracted_audio = f"audio-{input_video_name}.wav"
+    stream = ffmpeg.input(input_video)
+    stream = ffmpeg.output(stream, extracted_audio)
+    ffmpeg.run(stream, overwrite_output=True)
+    return extracted_audio
